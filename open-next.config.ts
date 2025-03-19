c95022362cd39fdcb153382021ec93f3b73baaaa
Brain <gsaboia@yahoo.com>
2025-03-18 21:54:38 -0300

    fix: added queue to open-next

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,8 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
 import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
-
+import memoryQueue from "@opennextjs/cloudflare/memory-queue";
 export default defineCloudflareConfig({
   incrementalCache: kvIncrementalCache,
+  queue: memoryQueue,
 });
