6d21e72c05821609780b41bd4d32217728267859
Brain <gsaboia@yahoo.com>
2025-03-26 14:13:15 -0300

    fix: opennext update

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,7 +1,7 @@
 // default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
-import memoryQueue from "@opennextjs/cloudflare/memory-queue";
+import kvIncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
+import memoryQueue from "@opennextjs/cloudflare/overrides/queue/memory-queue";
 export default defineCloudflareConfig({
   incrementalCache: kvIncrementalCache,
   queue: memoryQueue,
