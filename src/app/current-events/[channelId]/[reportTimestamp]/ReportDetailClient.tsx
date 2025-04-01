6689768e6b220bedeb5e71c390a8a378497349e4
Brain <gsaboia@yahoo.com>
2025-04-01 10:43:48 -0300

    fix: report detail page

--- src/app/current-events/[channelId]/[reportTimestamp]/ReportDetailClient.tsx
+++ src/app/current-events/[channelId]/[reportTimestamp]/ReportDetailClient.tsx
@@ -1,6 +1,9 @@
 "use client"
 
-import { Report } from "@/lib/types/core";
+import MessagesAccordion from "@/components/current-events/MessagesAccordion";
+import { DiscordMessage, Report } from "@/lib/types/core";
+import { formatTime } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -10,6 +13,7 @@ export default function ReportDetailClient() {
     const channelId = Array.isArray(params.channelId) ? params.channelId[0] : params.channelId;
 
     const [report, setReport] = useState<Report | null>(null);
+    const [messages, setMessages] = useState<DiscordMessage[]>([]);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -18,21 +22,34 @@ export default function ReportDetailClient() {
             const reportResponse = await fetch(`/api/report?channelId=${channelId}&reportId=${reportId}`);
             if (!reportResponse.ok) throw new Error('Failed to fetch reports');
             const data = await reportResponse.json();
-            setReport(data.report);
+            const { report, messages } = data;
+            setReport(report);
+            setMessages(messages);
             setIsLoading(false);
         }
         fetchReportAndMessages();
     }, [reportId, channelId]);
 
     return (
-        <div>
+        <div className="p-6 max-w-5xl mx-auto gap-4 flex flex-col">
+            <h3>{report?.channelName}</h3>
             {isLoading ? (
-                <div>Loading...</div>
+                <div className="flex items-center justify-center h-full">
+                    <Loader2 className="h-10 w-10 animate-spin text-primary" />
+                </div>
             ) : (
-                <div>
-                    <h1>{report?.headline}</h1>
-                    <p>{report?.body}</p>
-                    <p>{report?.city}</p>
+                <div className="flex flex-col gap-4">
+                    <h1 className="text-2xl font-bold">{report?.headline}</h1>
+                    <p className="text-muted-foreground">{formatTime(report?.timestamp, true)} - {report?.city}</p>
+                    <p className="prose prose-zinc max-w-none overflow-y-auto text-justify">{report?.body}</p>
+                    <MessagesAccordion
+                        channelData={{
+                            count: messages.length,
+                            messages,
+                            loading: isLoading
+                        }}
+                        isLoading={isLoading}
+                    />
                 </div>
             )}
         </div>
